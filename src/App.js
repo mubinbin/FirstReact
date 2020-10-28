d6f75c113611d02e25dbdd487df21203e59785a5
Binbin Mu <dinobunmu@gmail.com>
2020-10-28 17:34:22 -0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h2>Thing I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Clime Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
